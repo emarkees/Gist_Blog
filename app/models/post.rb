d54a20e73e9e47ac2b0dd334c5878f5cd54781e6
Adeyemi Olusola <rphillipse@gmail.com>
2023-08-25 09:00:34 -0600

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
