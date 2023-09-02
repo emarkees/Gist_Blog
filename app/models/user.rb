624a841f132de2420a13ec4664f9eda7eea99260
emarkees <rphillipse@gmail.com>
2023-09-02 07:09:48 -0600

    Update the post as requested by the PR

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
 
-  attribute :posts_counter, :integer, default: 0
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
