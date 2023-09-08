f6328fcc8651729ee56cd7cb3fdfb11fd95ded0d
emarkees <rphillipse@gmail.com>
2023-09-08 01:42:53 -0600

    Add N+1 problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-    @user = User.find(params[:id]) || not_found
+    @user = User.includes(:posts).find(params[:id]) || not_found
   end
 end
